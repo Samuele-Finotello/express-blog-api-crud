4edb3fc1c88e6711abecf514f64ed752061bfca8
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-08-04 17:16:27 +0200

    [add] feat creazione file routerPosts con il percorso dei metodi definiti in controllerPosts

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const routerPosts = require('./routers/routerPosts.js');
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Server del mio blog');
 })
+
+app.use('/posts', routerPosts);
 
 app.listen((port), () => {
   console.log(`Server in ascolto sulla porta ${port}`);
--- controllers/controllerPosts.js
+++ controllers/controllerPosts.js
@@ -1,25 +1,32 @@
+//creo posts che contiene tutti i post dell'array dentro posts.js
 const posts = require('../data/posts.js');
 
+//creo la funzione index che contiene la lista di tutti i post
 const index = (req, res) => {
   res.send('Lista di tutti i post');
 }
 
+//creo la funzione show che contiene tutti i dettagli del post preso in oggetto
 const show = (req, res) => {
   res.send('Dettagli di un post');
 }
 
+//creo la funzione store per creare un nuovo post
 const store = (req, res) => {
   res.send('Crea un nuovo post');
 }
 
+//creo la funzione update per aggiornare completamente un post
 const update = (req, res) => {
   res.send('Aggiorna interamente un post');
 }
 
+//creo la funzione modify per aggiornare solo una parte del post
 const modify = (req, res) => {
   res.send('Aggiorna parzialmente un post');
 }
 
+//creo la funzione destroy per eliminare un post
 const destroy = (req, res) => {
   res.send('Elimina un post');
 }
